ddd03a56d1243524e7e5d9445d739014d64d7955
Sarah <sarahs389@nycstudents.net>
2024-09-10 16:55:41 -0400

    lab 1

---
+++ Shao_Sarah_Lab_1.cpp
@@ -0,0 +1,22 @@
+// Sarah Shao
+// 09/10/2024
+// Lab 1 
+
+#include <iostream>
+
+using namespace std;   // shortcut
+
+int main() //to start the code
+{
+    cout << "################################################################################\n" 
+    "#                                                                              #\n"  // spaces after the hashtags
+    "#Ways to access the Task Manager on your Windows computer:                     #\n"  // first line
+    "#                                                                              #\n"  // spaces for box
+    "#       Press the key combination Ctrl + Shift + Escape                        #\n"
+    "#                                                                              #\n"  // spaces for box
+    "#       Press the key combination Ctrl + Alt + Delete and select “Task Manager”#\n"
+    "#                                                                              #\n"  // spaces for box
+    "#       Type “Task Manager” in the Windows Start menu search                   #\n"
+    "#                                                                              #\n"  // spaces before hashtags
+    "################################################################################" << endl;
+}
